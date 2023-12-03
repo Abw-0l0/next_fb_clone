76b279ab55e7aaadec91e9cedfc5a1b385267d27
Abdul Basit Iqbal <iamabdulbasit5@gmail.com>
2023-12-03 13:40:33 +0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,19 +1,19 @@
-import NextAuth from "next-auth";
+// import NextAuth from "next-auth";
 // import Providers from "next-auth/react";
-import Providers from "next-auth/providers/facebook"
+// import Providers from "next-auth/providers/facebook"
 // import FacebookProvider from "next-auth/providers/facebook";
 
-export default NextAuth({
+// export default NextAuth({
   // Configure one or more authentication providers
-  providers: [
-    Providers({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  // providers: [
+  //   Providers({
+  //     clientId: process.env.FACEBOOK_CLIENT_ID,
+  //     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
       // callbackURL: "http://localhost:3000"
-    }),
+    // }),
     // ...add more providers here
-  ],
-  secret: process.env.NEXT_PUBLIC_SECRET,
-});
+  // ],
+  // secret: process.env.NEXT_PUBLIC_SECRET,
+// });
 
 // export default NextAuth(authOptions)
